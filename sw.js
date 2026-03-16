171e442f66d086b714ab209fddf76f85a4d74efa
alessandrociapina-cpu <alessandro.ciapina@gmail.com>
2026-03-16 09:44:01 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+﻿const CACHE_NAME = 'sabesp-orcamento-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './servicos.js',
+  './materiais.js',
+  './logosabesp.png',
+  './manifest.json'
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
